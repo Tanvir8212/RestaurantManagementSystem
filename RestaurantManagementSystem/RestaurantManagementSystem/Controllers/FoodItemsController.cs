c843fbfbcfee04382d42de05581d4e1907c43d2e
sajid8212 <sajid8212@gmail.com>
2019-06-10 18:56:06 +0600

    Food Item & Category

---
+++ RestaurantManagementSystem/RestaurantManagementSystem/Controllers/FoodItemsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantManagementSystem.Models;
+
+namespace RestaurantManagementSystem.Controllers
+{
+    public class FoodItemsController : Controller
+    {
+        private RestaurantDbContext db = new RestaurantDbContext();
+
+        // GET: FoodItems
+        public ActionResult Index()
+        {
+            var foodItems = db.FoodItems.Include(f => f.Category);
+            return View(foodItems.ToList());
+        }
+
+        // GET: FoodItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodItem foodItem = db.FoodItems.Find(id);
+            if (foodItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foodItem);
+        }
+
+        // GET: FoodItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.FoodCategories, "Id", "Name");
+            return View();
+        }
+
+        // POST: FoodItems/Create
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Price,Description,CategoryId")] FoodItem foodItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FoodItems.Add(foodItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.FoodCategories, "Id", "Name", foodItem.CategoryId);
+            return View(foodItem);
+        }
+
+        // GET: FoodItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodItem foodItem = db.FoodItems.Find(id);
+            if (foodItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.FoodCategories, "Id", "Name", foodItem.CategoryId);
+            return View(foodItem);
+        }
+
+        // POST: FoodItems/Edit/5
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Price,Description,CategoryId")] FoodItem foodItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(foodItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.FoodCategories, "Id", "Name", foodItem.CategoryId);
+            return View(foodItem);
+        }
+
+        // GET: FoodItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodItem foodItem = db.FoodItems.Find(id);
+            if (foodItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foodItem);
+        }
+
+        // POST: FoodItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FoodItem foodItem = db.FoodItems.Find(id);
+            db.FoodItems.Remove(foodItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
